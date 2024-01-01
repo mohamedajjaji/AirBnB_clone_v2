b90173a7dad59b7415064940e032cac39ce70fbd
Mohamed AJJAJI <majjaji98@gmail.com>
2024-01-01 09:58:08 -0800

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Create a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+"""
+Define a route for the root URL ('/')
+and disable strict slashes """
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Return the desired message """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Return the desired message """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ Return 'C' followed by the value of <text> """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_with_text(text='is cool'):
+    """
+    Return 'Python' then the value of <text>
+    Replaces underscores in <text> with spaces """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ Return 'n is a number' only if 'n' is integer """
+    return '{} is a number'.format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """ Return an HTML page if 'n' is integer"""
+    path = '5-number.html'
+    return render_template(path, n=n)
+
+
+if __name__ == "__main__":
+    """ Start the Flask development server on 0.0.0.0:5000 """
+    app.run(host='0.0.0.0', port=5000)
